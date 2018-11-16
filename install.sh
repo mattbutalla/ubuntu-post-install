cb87525b2cd4531e1b9c792e3aa737798d6ba16a
Matt Butalla <butallmj@gmail.com>
2018-11-15 21:32:06 -0600

    zsh & oh my zsh

--- install.sh
+++ install.sh
@@ -91,3 +91,8 @@ dconf write "/org/gnome/settings-daemon/plugins/media-keys/home" "'<Super>e'"
 
 rm ~/.gitconfig
 ln -s $STARTING_DIR/dotfiles/.gitconfig ~/.gitconfig
+
+#zsh
+sudo apt-get -y install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
