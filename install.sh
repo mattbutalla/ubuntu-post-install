d918b931ccb58de89e3a633903ef14f15c4880d2
Matt Butalla <butallmj@gmail.com>
2018-11-20 14:21:24 -0600

    also install docker-compose

--- install.sh
+++ install.sh
@@ -33,7 +33,7 @@ sudo apt-get -y install apt-transport-https ca-certificates curl software-proper
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt-get update
-sudo apt-get -y install docker-ce
+sudo apt-get -y install docker-ce docker-compose
 sudo usermod -aG docker $USER
 
 #nodejs
